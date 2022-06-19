2d66a933fbbb3b371150f8e42ff138eb5dddb704
Yury Bayda <yury@bayda.pro>
2022-06-18 23:21:14 -0700

    Use std::string_view

--- main.h
+++ main.h
@@ -7,14 +7,14 @@
  * Utility function to split a string by the delimiter
  */
 inline std::vector<std::string>
-split(const std::string& record, char delimiter = ',')
+split(std::string_view record, char delimiter = ',')
 {
     std::vector<std::string> fields;
 
     size_t begin = 0, end;
     do {
         end = record.find_first_of(delimiter, begin);
-        fields.push_back(record.substr(begin, end - begin));
+        fields.emplace_back(record.substr(begin, end - begin));
         begin = end + 1;
     } while (end != std::string::npos);
 
